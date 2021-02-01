fa3334f321d1a9249e2f050deadb1b43a09ea77d
ED <emaddeve20@gmail.com>
2021-02-02 01:43:37 +0330

    improve code with pylint

--- p15.py
+++ p15.py
@@ -1,20 +1,35 @@
 #!/bin/env python3
 
+"""
+This code is puzzle 15 of Game!
+I wrote this code (EmadDeve20 in Github)
+email address: emaddeve20@gmail.com
+this is source have GPL LICENSE
+if you see a bug plz report it to me or pl I so happy about this :D
+or you have an idea plz send PL
+"""
 
+
+import os
+import platform
+import time
+import random
 from key_manager import controler
 from pnum import prmat
 import push_numbers as p
-import random , os , platform , time
 
 class Game:
-    """This is class For PlayGround"""
+    """This is class For PlayGround and Handle for win time"""
     def __init__(self):
-        self.mat = [[],[],[],[]]
-        numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+        self.mat = [[], [], [], []]
 
+        #numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+        numbers = []
+        for i in range(16):
+            numbers.append(i+1)
 
         for i in range(len(self.mat)):
-            for j in range(len(self.mat)):
+            for _j in range(len(self.mat)):
                 choice = random.choice(numbers)
                 self.mat[i].append(choice)
                 numbers.remove(choice)
@@ -23,25 +38,21 @@ class Game:
         #self.time_now = int(time.time()-self.time_start())
 
     def game_manager(self):
+        """this function sees the playground. The game is finished or Not!"""
         while p.matrix_is_alive(self.mat):
             self.draw()
             controler(self.mat)
         self.draw()
 
     def draw(self):
+        """this is function can draw a playground for player :D"""
         if platform.system() == "Linux":
             os.system("clear")
         elif platform.system() == 'Windows':
             os.system('cls')
         prmat(self.mat)
 
-    def not_finished(self):
-        for i in range(len(mat)):
-            for j in range(len(mat)):
-                if self.mat[i][j] > self.mat[i][j+1]:
-                    return True
-        return False
 
 if __name__ == "__main__":
-    game = Game()
-    game.game_manager()
+    GAME = Game()
+    GAME.game_manager()
