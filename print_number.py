f269a9c7faeb0d98c69c620058e548f59aa6d6e1
ED <emaddeve20@gmail.com>
2021-03-03 00:47:23 +0330

    improve code with pylint

--- print_number.py
+++ print_number.py
@@ -1,10 +1,23 @@
 #!/bin/env python3
 
+"""
+This Module for print Matrix of Game
+--------------
+1   2   3   4
+5   6   7   8
+9   10  11  12
+13  14  15
+--------------
+"""
+
+from typing import List
+
+
 def reset():
     """clear font color"""
     print("\033[0m")
 
-def prnum(number):
+def prnum(number: int):
     """this is function can be printing your number with private color"""
     try:
         number = int(number)
@@ -14,17 +27,18 @@ def prnum(number):
 
     bold = '\033[01m'
     if number == 16:
-        print(" \t" , end = '')
+        print(" \t", end='')
     else:
-        print(bold+str(number)+'\t' , end = '')
+        print(bold+str(number)+'\t', end='')
 
-def plist(numbers):
+def plist(numbers: List[int]):
     """this function can be priting a numbers with private colors"""
     for i in range(len(numbers)):
         prnum(numbers[i])
 
-def prmat(mat):
+def prmat(mat: List[List[int]]):
     """this is function can be priting matrix"""
     for i in range(len(mat)):
         plist(mat[i])
-        print("");reset()
+        print("")
+        reset()
